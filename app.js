eccdba336b4703c83ad75c624957b2422bd1bf1c
Valou0539 <87665858+Valou0539@users.noreply.github.com>
2021-08-14 13:50:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -275,6 +275,6 @@ loginInput.addEventListener('click', sendLoginForm)
 // Mobile #cgu::after position change
 
 if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    window.alert(vous êtes sur mobile)
+    window.alert('vous êtes sur mobile')
     signUpFormContent.cgu.classList.add('mobil')
 }
